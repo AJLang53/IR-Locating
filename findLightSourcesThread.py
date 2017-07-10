fa01efb87ea750d3f6ca2b3e9fe2d46121f564b5
Austin <AJLang53@htomail.com>
2017-07-10 13:36:55 -0400

    Added IMU stuff, other fixes

--- findLightSourcesThread.py
+++ findLightSourcesThread.py
@@ -9,41 +9,135 @@ from picamera.array import PiRGBArray
 from picamera import PiCamera
 import time
 import math
+import threading
+
+class getOrientation:
+
+    def __init__(self, sensor):
+        self.sensor = sensor
+        self.pitch = 0.0
+        self.roll = 0.0
+        self.yaw = 0.0
+        self.prevPitch = 0.0
+        self.prevRoll = 0.0
+        self.prevYaw = 0.0
+        self.end = False
+        if self.sensor == 'hat':
+            thread = threading.Thread(target=self.runHat,args=())
+        elif self.sensor == 'BNO':
+            thread = threading.Thread(target=self.runBNO,args=())
+            
+        thread.deamon = True
+        thread.start()
+
+
+    def runBNO(self):
+        bno = BNO055.BNO055(rst=18) 
+        if not bno.begin():
+            raise RuntimeError("Failed to initialize BNO055")
+        
+        # Print system status and self test result.
+        status, self_test, error = bno.get_system_status()
+        print('System status: {0}'.format(status))
+        print('Self test result (0x0F is normal): 0x{0:02X}'.format(self_test))
+        # Print out an error if system status is in error mode.
+        if status == 0x01:
+            print('System error: {0}'.format(error))
+            print('See datasheet section 4.3.59 for the meaning.')
+
+        # Print BNO055 software revision and other diagnostic data.
+        sw, bl, accel, mag, gyro = bno.get_revision()
+        print('Software version:   {0}'.format(sw))
+        print('Bootloader version: {0}'.format(bl))
+        print('Accelerometer ID:   0x{0:02X}'.format(accel))
+        print('Magnetometer ID:    0x{0:02X}'.format(mag))
+        print('Gyroscope ID:       0x{0:02X}\n'.format(gyro))
+
+        while not self.end:
+            
+            yaw, roll, pitch = bno.read_euler()
+            
+            while pitch > 180:
+                pitch -= 360
+            while roll > 180:
+                roll -= 360
+            while yaw > 180:
+                yaw -= 360
+
+            if not (pitch <= -360):
+                self.pitch = pitch
+
+            if not (roll <= -360):
+                self.roll = roll
+
+            if not (yaw <= -360):
+                print(yaw)
+                self.yaw = yaw
+
+##            self.pitch = self.lowPass(pitch, self.prevPitch, 50 , 10)
+##            self.roll = self.lowPass(roll, self.prevRoll, 50 , 10)
+##            self.yaw = self.lowPass(yaw, self.prevYaw, 50 ,10)
+##
+            time.sleep(0.1)
+##
+##            self.prevPitch = self.pitch
+##            self.prevRoll = self.roll
+##            self.prevYaw = self.yaw
+                
+        
+    def runHat(self):
+        
+        senseHat = SenseHat()
+        samples = 5
+
+        while not self.end:
+            cumPitch = 0
+            cumRoll = 0
+            cumYaw = 0
+            for i in range(samples):
+                orient = senseHat.get_orientation()
+                cumPitch += orient['pitch']
+                cumRoll += orient['roll']
+                cumYaw += orient['yaw']
+
+            pitch = cumPitch/samples
+            roll = cumRoll/samples
+            yaw = cumYaw/samples
+            
+            while pitch > 180:
+                pitch -= 360
+            while roll > 180:
+                roll -= 360
+            while yaw > 180:
+                yaw -= 360
+
+            self.pitch = self.lowPass(pitch, self.prevPitch, 50 , 10)
+            self.roll = self.lowPass(roll, self.prevRoll, 50 , 10)
+            self.yaw = self.lowPass(yaw, self.prevYaw, 50 ,10)
+
+            time.sleep(0.01)
+
+            self.prevPitch = self.pitch
+            self.prevRoll = self.roll
+            self.prevYaw = self.yaw
+
+    def lowPass(self,x, y_old, rc, dt):
+        alpha = float(dt)/(rc+ dt)
+        return alpha*x + (1- alpha)*(y_old)
 
 def getAbsAngles(relAz,relEl,IMU):
     """
     Takes in camera relative rotations, and returns absolute rotations
     using the IMU
     """
-    if IMU.lower() == "bno":
-        yaw, roll, pitch = BNO055.read_euler()
-    
-    elif IMU.lower() == "hat":
-        samples = 5
-        cumPitch = 0
-        cumRoll = 0
-        cumYaw = 0
-        for i in range(samples):
-            orient = senseHat.get_orientation()
-            cumPitch += orient['pitch']
-            cumRoll += orient['roll']
-            cumYaw += orient['yaw']
 
-        pitch = cumPitch/samples
-        roll = cumRoll/samples
-        yaw = cumYaw/samples
+    pitch = orient.pitch
+    roll = orient.roll
+    yaw = orient.yaw
         
-        while pitch > 180:
-            pitch -= 360
-        while roll > 180:
-            roll -= 360
-        while yaw > 180:
-            yaw -= 360
-
-        print( yaw, roll, pitch)
-
     # Use the roll to transform the relative azimuth and elevation in
-    # the image into absolute azimuth and elevation (no roll)
+    # the image into absolute azimuth and elevation (no roll) (I'm not sure if
+    # this is correct)
     camAz = relAz*math.cos(roll*180/math.pi) - relEl*math.sin(roll*180/math.pi)
     camEle = relAz*math.sin(roll*180/math.pi) - relEl*math.cos(roll*180/math.pi)
     
@@ -85,9 +179,6 @@ def findLightSources(frame,threshold):
     sources = []
     # loop over the unique components
     for label in np.unique(labels):
-        # if this is the background label, ignore it
-##        if label == 0:
-##            continue
 
         # otherwise, construct the label mask and count the number of pixels 
         labelMask = np.zeros(thresh.shape, dtype="uint8")
@@ -126,10 +217,12 @@ if __name__ == "__main__":
         if args["imu"].lower() == 'hat':
             useHat = True
             from sense_hat import SenseHat
-            senseHat = SenseHat()
+            orient = getOrientation('hat')
         if args["imu"].lower() == 'bno':
             useBNO = True
             from Adafruit_BNO055 import BNO055
+            orient = getOrientation('BNO')
+            
         
     # Pic Properties
     picWidth = 640
@@ -194,4 +287,5 @@ if __name__ == "__main__":
 
         # if the `q` key was pressed, break from the loop
         if key == ord("q"):
+            orient.end = True
             break
